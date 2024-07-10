e5660eea83c0ee732c39f9791677c31d11dd9360
mesqueeb <mesqueeb@users.noreply.github.com>
2024-07-10 17:24:44 +0900

    tests: use new Swift Testing

--- MicroMaxOnAppleSilicon/Tests/MicroMaxOnAppleSiliconTests.swift
+++ MicroMaxOnAppleSilicon/Tests/MicroMaxOnAppleSiliconTests.swift
@@ -1,5 +1,5 @@
 @testable import MicroMaxOnAppleSilicon
-import XCTest
+import Testing
 
 struct FileRank: Equatable {
   let file: Int
@@ -21,67 +21,64 @@ struct FileRank: Equatable {
   }
 }
 
-class MicroMaxOnAppleSiliconTests: XCTestCase {
-  var bridge: MicroMaxBridge!
-
-  override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    bridge = MicroMaxBridge()
+final class Tests {
+  var bridge: MicroMaxBridge
+  
+  init() async throws {
+    self.bridge = MicroMaxBridge()
     bridge.connectToEngine()
   }
-
-  override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  
+  deinit {}
+  
+  @Test func coordinateToFileRank() throws {
+    #expect(FileRank(MicroMaxOnAppleSilicon.coordinateToFileRank(.B1)) == FileRank(file: 1, rank: 0))
+    #expect(FileRank(MicroMaxOnAppleSilicon.coordinateToFileRank(.B3)) == FileRank(file: 1, rank: 2))
+    #expect(FileRank(MicroMaxOnAppleSilicon.coordinateToFileRank(.C3)) == FileRank(file: 2, rank: 2))
+    #expect(FileRank(MicroMaxOnAppleSilicon.coordinateToFileRank(.D2)) == FileRank(file: 3, rank: 1))
+    #expect(FileRank(MicroMaxOnAppleSilicon.coordinateToFileRank(.D3)) == FileRank(file: 3, rank: 2))
+    #expect(FileRank(MicroMaxOnAppleSilicon.coordinateToFileRank(.D8)) == FileRank(file: 3, rank: 7))
+    #expect(FileRank(MicroMaxOnAppleSilicon.coordinateToFileRank(.H4)) == FileRank(file: 7, rank: 3))
   }
-
-  func testCoordinateToFileRank() {
-    XCTAssertEqual(FileRank(coordinateToFileRank(.B1)), FileRank(file: 1, rank: 0))
-    XCTAssertEqual(FileRank(coordinateToFileRank(.B3)), FileRank(file: 1, rank: 2))
-    XCTAssertEqual(FileRank(coordinateToFileRank(.C3)), FileRank(file: 2, rank: 2))
-    XCTAssertEqual(FileRank(coordinateToFileRank(.D2)), FileRank(file: 3, rank: 1))
-    XCTAssertEqual(FileRank(coordinateToFileRank(.D3)), FileRank(file: 3, rank: 2))
-    XCTAssertEqual(FileRank(coordinateToFileRank(.D8)), FileRank(file: 3, rank: 7))
-    XCTAssertEqual(FileRank(coordinateToFileRank(.H4)), FileRank(file: 7, rank: 3))
+  
+  @Test func fileRankToCoordinate() throws {
+    #expect(MicroMaxOnAppleSilicon.fileRankToCoordinate(file: 1, rank: 0) == .B1)
+    #expect(MicroMaxOnAppleSilicon.fileRankToCoordinate(file: 1, rank: 2) == .B3)
+    #expect(MicroMaxOnAppleSilicon.fileRankToCoordinate(file: 2, rank: 2) == .C3)
+    #expect(MicroMaxOnAppleSilicon.fileRankToCoordinate(file: 3, rank: 1) == .D2)
+    #expect(MicroMaxOnAppleSilicon.fileRankToCoordinate(file: 3, rank: 2) == .D3)
+    #expect(MicroMaxOnAppleSilicon.fileRankToCoordinate(file: 3, rank: 7) == .D8)
+    #expect(MicroMaxOnAppleSilicon.fileRankToCoordinate(file: 7, rank: 3) == .H4)
   }
-
-  func testFileRankToCoordinate() {
-    XCTAssertEqual(fileRankToCoordinate(file: 1, rank: 0), .B1)
-    XCTAssertEqual(fileRankToCoordinate(file: 1, rank: 2), .B3)
-    XCTAssertEqual(fileRankToCoordinate(file: 2, rank: 2), .C3)
-    XCTAssertEqual(fileRankToCoordinate(file: 3, rank: 1), .D2)
-    XCTAssertEqual(fileRankToCoordinate(file: 3, rank: 2), .D3)
-    XCTAssertEqual(fileRankToCoordinate(file: 3, rank: 7), .D8)
-    XCTAssertEqual(fileRankToCoordinate(file: 7, rank: 3), .H4)
-  }
-
-  func testMoveLegality() {
+  
+  @Test func moveLegality() throws {
     // Test 1.Nc3 from the starting position (Expected: True)
-    XCTAssertTrue(bridge.isMoveLegal("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", from: .B1, to: .C3), "1.Nc3 should be legal")
-
+    #expect(bridge.isMoveLegal("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", from: .B1, to: .C3), "1.Nc3 should be legal")
+  
     // Test 1.Nb3 from the starting position (Expected: False)
-    XCTAssertFalse(bridge.isMoveLegal("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", from: .B1, to: .B3), "1.Nb3 should be illegal")
-
+    #expect(!bridge.isMoveLegal("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", from: .B1, to: .B3), "1.Nb3 should be illegal")
+  
     // Test 1.d4 from the starting position (Expected: True)
-    XCTAssertTrue(bridge.isMoveLegal("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", from: .D2, to: .D3), "1.d4 should be legal")
-
+    #expect(bridge.isMoveLegal("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", from: .D2, to: .D3), "1.d4 should be legal")
+  
     // Test 1...Qh4 illegal move (Expected: False)
-    XCTAssertFalse(bridge.isMoveLegal("rnbqkb1r/pppp1ppp/4pn2/8/6P1/8/PPPPPP1P/RNBQKBNR b KQkq - 0 1", from: .D8, to: .H4), "1...Qh4 should be illegal in this position")
-
+    #expect(!bridge.isMoveLegal("rnbqkb1r/pppp1ppp/4pn2/8/6P1/8/PPPPPP1P/RNBQKBNR b KQkq - 0 1", from: .D8, to: .H4), "1...Qh4 should be illegal in this position")
+  
     // Test 1...Qh4 legal move (Expected: True)
-    XCTAssertTrue(bridge.isMoveLegal("rnbqkbnr/pppp1ppp/4p3/8/6P1/8/PPPPPP1P/RNBQKBNR b KQkq - 0 1", from: .D8, to: .H4), "1...Qh4 should be legal in this position")
+    #expect(bridge.isMoveLegal("rnbqkbnr/pppp1ppp/4p3/8/6P1/8/PPPPPP1P/RNBQKBNR b KQkq - 0 1", from: .D8, to: .H4), "1...Qh4 should be legal in this position")
   }
-
-  func testGameStatus() {
+  
+  @Test func gameStatus() throws {
     // Starting position should be normal (Expected: 0)
-    XCTAssertEqual(bridge.getGameStatus("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"), GameStatus.normal, "Starting position should be normal")
-
+    #expect(bridge.getGameStatus("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") == GameStatus.normal, "Starting position should be normal")
+    
     // Checkmated position (Expected: 1)
-    XCTAssertEqual(bridge.getGameStatus("4R1k1/5ppp/8/8/8/8/5PPP/6K1 b - - 0 1"), GameStatus.checkmated, "Position should be checkmate")
-
+    #expect(bridge.getGameStatus("4R1k1/5ppp/8/8/8/8/5PPP/6K1 b - - 0 1") == GameStatus.checkmated, "Position should be checkmate")
+    
     // Stalemate position (Expected: 2)
-    XCTAssertEqual(bridge.getGameStatus("6k1/6P1/6KP/8/8/8/8/8 b - - 0 1"), GameStatus.stalemate, "Position should be stalemate")
-
+    #expect(bridge.getGameStatus("6k1/6P1/6KP/8/8/8/8/8 b - - 0 1") == GameStatus.stalemate, "Position should be stalemate")
+    
     // Normal position (Expected: 0)
-    XCTAssertEqual(bridge.getGameStatus("6k1/6P1/6KP/8/8/8/8/8 w - - 0 1"), GameStatus.normal, "Position should be normal")
+    #expect(bridge.getGameStatus("6k1/6P1/6KP/8/8/8/8/8 w - - 0 1") == GameStatus.normal, "Position should be normal")
   }
 }
